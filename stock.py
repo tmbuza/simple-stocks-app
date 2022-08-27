088a261d6b7bc5e3bfdb03affaf1604ecfddba1a
Teresia Mrema-Buza <ndelly@gmail.com>
2022-08-26 23:12:50 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -25,7 +25,8 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-1-31')
 
 """
-![Table 1: A typical stock data structure](img/stock.png)
+<img src="img/stock.png" alt="Book cover" width="100%" style="padding: 50px 0px 50px 0px; float: right;"/>
+![](img/stock.png)
 """
 
 tickerDf.head(10).to_csv('stockhead.csv')
